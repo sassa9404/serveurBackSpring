53de0b8fbdd7766d5bb8c9c13f3689cebf5b06ad
sassa9404 <safae.lachgar@sncf.fr>
2023-10-10 16:46:45 +0200

    test

---
+++ src/main/java/com/example/ServeurBack/ServeurBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServeurBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ServeurBack/ServeurBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServeurBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
